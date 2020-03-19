0278e480f999bdd78e18a695383fb0cd7cfa35d3
Eliel Dabush <eliel@jolt.us>
2020-03-19 17:13:44 +0200

    add storybook & update skills

---
+++ .storybook/main.js
@@ -0,0 +1,6 @@
+import "../src/css/style.css";
+
+module.exports = {
+    stories: ["../src/stories/**/*.stories.js"],
+    addons: ["@storybook/addon-actions", "@storybook/addon-links"]
+};
---
+++ src/stories/10-lists-levels.stories.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ListLevels from "../components/sections/list-levels";
+import {jobs} from "../utils/data/experiences/jobs";
+
+export default {
+    title: "List Levels",
+    component: ListLevels
+};
+
+const items = [
+    {
+        name: "Sing",
+        level: 1
+    },
+    {
+        name: "Draw",
+        level: 2
+    },
+    {
+        name: "Design",
+        level: 3
+    },
+    {
+        name: "Music",
+        level: 4
+    },
+    {
+        name: "Development",
+        level: 5
+    }
+];
+
+export const LevelsList = () => <ListLevels items={items} title="My Levels List" />;
